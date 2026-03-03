ba462ab0f1299bb05a2c6269d390fe1eaba65270
sunghwanism <97149338+sunghwanism@users.noreply.github.com>
2026-03-03 13:53:26 -0500

    init reference data

---
+++ dataset/reference/residue_dictionary.py
@@ -0,0 +1,49 @@
+residue3to1 = {
+    "ALA": "A",  # Alanine
+    "ARG": "R",  # Arginine
+    "ASN": "N",  # Asparagine
+    "ASP": "D",  # Aspartic acid
+    "CYS": "C",  # Cysteine
+    "GLN": "Q",  # Glutamine
+    "GLU": "E",  # Glutamic acid
+    "GLY": "G",  # Glycine
+    "HIS": "H",  # Histidine
+    "ILE": "I",  # Isoleucine
+    "LEU": "L",  # Leucine
+    "LYS": "K",  # Lysine
+    "MET": "M",  # Methionine
+    "PHE": "F",  # Phenylalanine
+    "PRO": "P",  # Proline
+    "SER": "S",  # Serine
+    "THR": "T",  # Threonine
+    "TRP": "W",  # Tryptophan
+    "TYR": "Y",  # Tyrosine
+    "VAL": "V",  # Valine
+    "SEC": "U",  # Selenocysteine
+    "PYL": "O",  # Pyrrolysine
+}
+
+residue1to3 = {
+    "A": "ALA",
+    "R": "ARG",
+    "N": "ASN",
+    "D": "ASP",
+    "C": "CYS",
+    "Q": "GLN",
+    "E": "GLU",
+    "G": "GLY",
+    "H": "HIS",
+    "I": "ILE",
+    "L": "LEU",
+    "K": "LYS",
+    "M": "MET",
+    "F": "PHE",
+    "P": "PRO",
+    "S": "SER",
+    "T": "THR",
+    "W": "TRP",
+    "Y": "TYR",
+    "V": "VAL",
+    "U": "SEC",
+    "O": "PYL",
+}
