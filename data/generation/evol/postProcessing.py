63d80c3b4c1636a657095a01d62429243d474711
sunghwanism <sunghwan.moon.ai@gmail.com>
2026-04-02 15:20:11 -0400

    fix bug: HMM Post processing code

--- data/generation/evol/postProcessing.py
+++ data/generation/evol/postProcessing.py
@@ -1,10 +1,13 @@
 import os
-import json
-
 import argparse
+
+import subprocess
+from concurrent.futures import ProcessPoolExecutor
 
 def generate_missing_hhm(args):
     fasta_path, db_path, output_hhm = args
+
+    db_path = os.path.join(db_path, "UniRef30_2023_02")
     
     cmd = [
         "hhblits",
@@ -12,7 +15,7 @@ def generate_missing_hhm(args):
         "-d", db_path,
         "-ohhm", output_hhm,
         "-n", "2",
-        "-cpu", str(args.workers),
+        "-cpu", "4",
         "-v", "1"
     ]
     
@@ -25,7 +28,7 @@ def generate_missing_hhm(args):
 
 def main(args):
 
-    os.makedirs(hhm_output_dir, exist_ok=True)
+    os.makedirs(args.hhm_output_dir, exist_ok=True)
 
     tasks = []
     for fasta_file in os.listdir(args.fasta_dir):
@@ -39,7 +42,7 @@ def main(args):
 
     print(f"Generating HHM for {len(tasks)} missing proteins...")
 
-    with ProcessPoolExecutor(max_workers=MAX_WORKERS) as executor:
+    with ProcessPoolExecutor(max_workers=args.workers) as executor:
         results = list(executor.map(generate_missing_hhm, tasks))
 
     for res in results:
@@ -52,7 +55,7 @@ if __name__ == "__main__":
     parser.add_argument("--fasta_dir", type=str, required=True, help="path to fasta directory for saving")
     parser.add_argument("--uniref_db_path", type=str, help="path to uniref database")
     parser.add_argument("--hmm_dir", type=str, help="path to hmm directory for saving")
-    parser.add_argument("--workers", type=int, default=os.cpu_count(), help="number of parallel workers")
+    parser.add_argument("--workers", type=int, required=True, help="number of parallel workers")
     parser.add_argument('--jobs', nargs='+', choices=['pssm', 'hmm'], help="jobs to run [pssm, hmm]")
     parser.add_argument('--hhm_output_dir', type=str, required=True, help="path to hhm output directory for saving")
     args = parser.parse_args()
